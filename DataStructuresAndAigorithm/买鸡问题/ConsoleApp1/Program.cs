f4b1dc2c73c99b5d39a7cf4f19bf3cac06ee519c
亚索 <58197887+abvdu@users.noreply.github.com>
2019-12-31 09:26:33 +0800

    算法问题
    年龄姓氏问题，百鸡问题，数据结构定义

---
+++ DataStructuresAndAigorithm/买鸡问题/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for(int i=0;i<20;i++)
+            {
+                for(int j=0;j<33;j++)
+                {
+
+                    for (int d = 0; d <= 100; d = d + 3)
+                    {
+
+                        if (d + j + i == 100 && i * 5 + j * 3 + d/3 ==100)
+                        {
+                            Console.WriteLine(i);
+                            Console.WriteLine(j);
+                            Console.WriteLine(d);
+                        }
+                    }
+                }
+            }
+           
+            Console.Read();
+        }
+    }
+}
