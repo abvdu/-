915aac00cf79ca73f084c731bcf21c59ae99e44c
亚索 <58197887+abvdu@users.noreply.github.com>
2020-01-07 10:25:49 +0800

    字典集合
    字典集合

---
+++ DataStructuresAndAigorithm/字典集合元素/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS006.IDictionaryDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 创建字典并添加元素
+            IDictionary d = new SimpleDictionary(3);
+            d.Add("小李", 40);
+            d.Add("小丽", 34);
+            d.Add("晓丽", 18);
+
+            Console.WriteLine("字典中的元素数量 = {0}", d.Count);
+            Console.WriteLine("字典元素是否包含 '小李'? {0}", d.Contains("小李"));
+            Console.WriteLine("韦小宝的年龄是 {0}", d["小李"]);
+
+            // 显示所有的 key/value 对
+            foreach (DictionaryEntry de in d)
+            {
+                Console.WriteLine("{0} 的年龄是 {1} 。", de.Key, de.Value);
+            }
+
+            // 移除：字典中有的
+            d.Remove("小李");
+
+            // 移除：字典中没有的
+            d.Remove("小张");
+
+            foreach (String s in d.Keys)
+                Console.WriteLine(s);
+
+            foreach (Int32 age in d.Values)
+                Console.WriteLine(age);
+
+            Console.ReadKey();
+        }
+    }
+}
